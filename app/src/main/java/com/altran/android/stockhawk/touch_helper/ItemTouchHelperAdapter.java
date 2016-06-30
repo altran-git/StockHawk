3b94675a89fd099ed581c4d5b96ca005fed58621
Andy Tran <Andy Tran>
2016-06-30 13:34:32 -0700

    Initial commit

---
+++ app/src/main/java/com/altran/android/stockhawk/touch_helper/ItemTouchHelperAdapter.java
@@ -0,0 +1,11 @@
+package com.altran.android.stockhawk.touch_helper;
+
+/**
+ * Created by altran on 10/6/15.
+ * credit to Paul Burke (ipaulpro)
+ * Interface to enable swipe to delete
+ */
+public interface ItemTouchHelperAdapter {
+
+  void onItemDismiss(int position);
+}
